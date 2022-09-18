8f4ffb30d87a9400b4f8fe083a9a66aff5a51df0
Yany954 <yanygonzalezyepez@gmail.com>
2022-09-17 23:10:33 -0500

    prueba1

--- RegistroEmpleados/src/registroempleados/Empleado.java
+++ RegistroEmpleados/src/registroempleados/Empleado.java
@@ -10,5 +10,57 @@ package registroempleados;
  * @author yanyg
  */
 public class Empleado {
-    //datos del empleado
+    String nombre;
+    int codigo;
+    boolean tipoEmpleado;
+    int meses;
+
+    public Empleado(String nombre, int codigo, boolean tipoEmpleado, int meses) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.tipoEmpleado = tipoEmpleado;
+        this.meses = meses;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public boolean isTipoEmpleado() {
+        return tipoEmpleado;
+    }
+
+    public void setTipoEmpleado(boolean tipoEmpleado) {
+        this.tipoEmpleado = tipoEmpleado;
+    }
+
+    public int getMeses() {
+        return meses;
+    }
+
+    public void setMeses(int meses) {
+        this.meses = meses;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "nombre=" + nombre + ", codigo=" + codigo + ", tipoEmpleado=" + tipoEmpleado + ", meses=" + meses + '}';
+    }
+    
+    
 }
+    //datos del empleado
+    //to string -> valores  
+
